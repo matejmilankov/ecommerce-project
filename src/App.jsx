3c60d67fce8d42fbb87498dfbe5f6b4582f16327
Matej Milankov <matej.milankov@gmail.com>
2026-06-12 16:36:40 +0200

    Move tracking page into React

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { HomePage } from './pages/HomePage';
 import { CheckoutPage } from './pages/CheckoutPage';
 import { OrdersPage } from './pages/OrdersPages';
+import { TrackingPage } from './pages/TrackingPage';
 import { Route, Routes } from 'react-router';
 import './App.css'
 
@@ -10,6 +11,7 @@ function App() {
       <Route path='/' element={<HomePage/>} />
       <Route path='checkout' element={<CheckoutPage/>} />
       <Route path='orders' element={<OrdersPage/>} />
+      <Route path='tracking' element={<TrackingPage/>}/>
     </Routes>
   );
 }
